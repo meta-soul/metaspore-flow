780c537bc9145282b6b4299ecade80c35be9849b
qinyy907 <yuanyq@dmetasoul.com>
2022-09-29 19:29:41 +0800

    add online code

--- python/metasporeflow/online/check_service.py
+++ python/metasporeflow/online/check_service.py
@@ -2,6 +2,9 @@ import requests
 
 
 def checkRecommendService(host="127.0.0.1", port=9081):
-    resp = requests.get('http://%s:%s/actuator/health/dataSource' % (host, port))
-    data = resp.json()
-    return data.get('status', "OUT_OF_SERVICE") == "UP"
+    try:
+        resp = requests.get('http://%s:%s/actuator/health/dataSource' % (host, port))
+        data = resp.json()
+        return data.get('status', "OUT_OF_SERVICE") == "UP"
+    except:
+        return False
