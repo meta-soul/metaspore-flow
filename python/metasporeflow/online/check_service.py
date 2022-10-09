4ec0659e661432f1d5d960d392100e46061bac2f
yuanyq <yuanyq@dmetasoul.com>
2022-10-09 10:50:39 +0800

    add refresh resp

--- python/metasporeflow/online/check_service.py
+++ python/metasporeflow/online/check_service.py
@@ -2,8 +2,8 @@ import requests
 
 def notifyRecommendService(host="127.0.0.1", port=8081):
     resp = requests.post('http://%s:%s/actuator/refresh' % (host, port))
-    data = resp.json()
-    print(data)
+    #data = resp.json()
+    #print(data)
 
 if __name__ == "__main__":
     print("test")
